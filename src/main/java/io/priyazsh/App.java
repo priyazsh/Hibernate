12687ccb9ae361aba19116e1afc5f1b97417cf62
Priyansh Prajapat <priyanshprajapat@proton.me>
2026-02-15 10:04:47 +0530

    init maven project

---
+++ src/main/java/io/priyazsh/App.java
@@ -0,0 +1,13 @@
+package io.priyazsh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
